8cbd4f6cbec250c89f3018d821cc6347a76e15a8
Maxime <Maxime@Maximinou>
2014-01-23 18:23:28 +0100

    Test base sur les thèmes

--- MarathonWeb/vue/Vue.php
+++ MarathonWeb/vue/Vue.php
@@ -7,19 +7,19 @@ class Vue {
     public function __construct($param) {
         $this->obj = $param;
     }
-    
+
     public function afficherplat() {
         include 'html/header.html';
         echo "<h1 class=\"ptitre\">" . $this->obj->nom . "</h1>";
-        
+
         echo '<div class="dimage"><figure><a href="others/images/originales/' . $this->obj->photo . '" target="_blank"><img src="others/images/petites/' . $this->obj->photo . '" alt="" /></a><figcaption><a href="others/images/originales/' . $this->obj->photo . '" target="_blank">Cliquez ici ou sur l\'image pour voir l\'image originale (plus grand)</a></figcaption></figure></div>';
         include 'html/footer.html';
     }
-    
+
     public function vuedefault() {
         include 'html/header.html';
         include ('html/accueil.html');
-        foreach($this->obj as $t) {
+        foreach ($this->obj as $t) {
             echo $this->vue_theme($t);
         }
         include 'html/footer.html';
@@ -27,12 +27,12 @@ class Vue {
 
     public function vue_all_theme() {
         include 'html/header.html';
-        foreach($this->obj as $t) {
+        foreach ($this->obj as $t) {
             echo $this->vue_theme($t);
         }
         include 'html/footer.html';
     }
-    
+
     public function vue_theme($theme) {
         $res = "<div class=\"contenu\">\n";
         $id = $theme->id;
@@ -42,15 +42,17 @@ class Vue {
         $res = $res . "</div>";
         return $res;
     }
-    
-    public function vue_all_resto() {
+
+    public function vue_all_resto($idtheme) {
         include 'html/header.html';
-        foreach($this->obj as $r) {
+        $t = Theme::findById($idtheme);
+        echo '<a href="' . $t->getUrl() . '">' . $t->nom . '</a><br/>';
+        foreach ($this->obj as $r) {
             echo $this->vue_resto($r);
         }
         include 'html/footer.html';
     }
-    
+
     public function vue_resto($resto) {
         $res = "<div class=\"contenu\">\n";
         $id = $resto->id;
@@ -58,18 +60,18 @@ class Vue {
 
         $res = $res . "<p class=catdescription>" . $resto->description . "</p>";
         $res = $res . "</div>";
-        return $res;  
+        return $res;
     }
-    
+
     public function vue_all_plat() {
         include 'html/header.html';
         $plat = $this->obj;
-        foreach($this->obj as $p) {
+        foreach ($this->obj as $p) {
             echo $this->vue_plat($p);
         }
         include 'html/footer.html';
     }
-    
+
     public function vue_plat($plat) {
         $res = "<div class=\"contenu\">\n";
         $id = $plat->id;
@@ -78,18 +80,19 @@ class Vue {
         $res = $res . "<p class=catdescription>" . $plat->description . "</p>";
         $res = $res . '<p class="lienpanier"><a href="panier.php?a=addPanier&idPlat=' . $plat->id . '&qte=' . 1 . '">Ajouter au panier</a></p></br>';
         $res = $res . "</div>";
-        return $res;  
+        return $res;
     }
-    
+
     public function vue_panier() {
         include 'html/header.html';
         $panier = $this->obj;
         $res = '<div id=panier><table><tr class="headerrow"><td>Theme</td><td>Plat</td><td>Restaurant</td><td>Quantité</td><td>P.U.</td><td>Total</td><td>Supprimer</td></tr>';
         foreach ($panier as $p) {
-            $res = $res . '<tr><td>'. $p['type'] .'</td><td>'. $p['plat'] .'</td><td>'. $p['restaurant'] .'</td><td>'. $p['nbre'] .'</td><td>'. $p['pu'] .'</td><td>'. $p['total'] .'</td><td>XXX</td></tr>';
+            $res = $res . '<tr><td>' . $p['type'] . '</td><td>' . $p['plat'] . '</td><td>' . $p['restaurant'] . '</td><td>' . $p['nbre'] . '</td><td>' . $p['pu'] . '</td><td>' . $p['total'] . '</td><td>XXX</td></tr>';
         }
         $res = $res . '</table></div><br/><a href="index.php">Retour à l\'accueil</a>';
         echo $res;
         include 'html/footer.html';
     }
+
 }
