0f0b24ad215f16fe02c13c5b142494f34cb9587e
kernel57 <kernel2.6.31@gmail.com>
2014-01-23 14:21:36 +0100

    Kernel57

--- MarathonWeb/base/Panier.php
+++ MarathonWeb/base/Panier.php
@@ -5,30 +5,24 @@ class Panier {
     public static function addPanier($plat, $nb=1) {
         if (!isset($_SESSION['panier'])) {
             $_SESSION['panier'] = array();
-            $_SESSION['panier']['plat'] = array();
-            $_SESSION['panier']['nbre'] = array();
+            $_SESSION['panier'][$plat->id] = array();
         }
-        $_SESSION['panier']['plat'][$plat->id] = $plat;
-        $_SESSION['panier']['nbre'][$plat->id] = $nb;
+        $_SESSION['panier'][$plat->id]['plat'] = $plat;
+        $_SESSION['panier'][$plat->id]['nbre'] = $nb;
     }
 
     public static function getDetailPanier() {
         $res = NULL;
         if (isset($_SESSION['panier'])) {
             $res = array();
-            $res['type'] = array();
-            $res['restaurant'] = array();
-            $res['plat'] = array();
-            $res['nbre'] = array();
-            $res['pu'] = array();
-            $res['total'] = array();
             foreach ($_SESSION['panier']['plat'] as $plat) {
-                $res['type'][$plat->id] = Theme::findById(Restaurant::findById($plat->id_resto)->id_theme)->nom;
-                $res['restaurant'][$plat->id] = Restaurant::findById($plat->id_resto)->nom;
-                $res['plat'][$plat->id] = $plat->nom;
-                $res['nbre'][$plat->id] = $_SESSION['panier']['nbre'][$plat->id];
-                $res['pu'][$plat->id] = $plat->prix;
-                $res['total'][$plat->id] = $plat->prix * $_SESSION['panier']['nbre'][$plat->id];
+                $res[$plat->id] = array();
+                $res[$plat->id]['type'] = Theme::findById(Restaurant::findById($plat->id_resto)->id_theme)->nom;
+                $res[$plat->id]['restaurant'] = Restaurant::findById($plat->id_resto)->nom;
+                $res[$plat->id]['plat'] = $plat->nom;
+                $res[$plat->id]['nbre'] = $_SESSION['panier']['nbre'][$plat->id];
+                $res[$plat->id]['pu'] = $plat->prix;
+                $res[$plat->id]['total'] = $plat->prix * $_SESSION['panier']['nbre'][$plat->id];
             }
         }
         return $res;
