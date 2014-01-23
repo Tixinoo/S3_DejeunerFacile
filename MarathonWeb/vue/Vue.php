00277bc4222c29a687b5789f0757885f03b166ef
Tixinoo <antoine.nosal@gmail.com>
2014-01-23 11:54:48 +0100

    vue avancement

--- MarathonWeb/vue/Vue.php
+++ MarathonWeb/vue/Vue.php
@@ -2,16 +2,28 @@
 
 class Vue {
 
-    private $controller;
+    private $obj;
 
     public function __construct($param) {
-        $this->controller = $param;
+        $this->obj = $param;
     }
 
     public function vue_general($param) {
         include 'html/header.html';
-        echo "coucou";
+        $all_themes = Theme::findAll();
+        foreach($all_themes as $t) {
+            vue_theme($t);
+        }
         include 'html/footer.html';
+    }
+    
+    public function vue_theme($theme) {
+        $res = "<div id=\"contenu\">\n";
+        $res = $res . "<h1>" . $theme->id . " : " . $theme->nom . "</h1>";
+
+        $res = $res . "<p id=catdescription>" . $theme->description . "</p>";
+        $res = $res . "</div>";
+        return $res;
     }
     
 }
