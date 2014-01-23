5ad525c82114e6812ca71aa9304817c2be169e88
kernel57 <kernel2.6.31@gmail.com>
2014-01-23 15:16:21 +0100

    Kernel57

--- MarathonWeb/vue/Vue.php
+++ MarathonWeb/vue/Vue.php
@@ -61,10 +61,8 @@ class Vue {
         return $res;  
     }
     
-    public function vue_panier() {
+    public function vue_panier($panier) {
         $res = '<div id=panier>\n<table><tr><td>Theme</td><td>Plat</td><td>Restaurant</td><td>Quantité</td><td>P.U.</td><td>Total</td></tr>';
-        
-        $panier = Panier::getDetailPanier();
         foreach ($panier as $p) {
             $res = $res . '<tr><td>'. $p->type .'</td><td>'. $p->restaurant .'</td><td>'. $p->plat .'</td><td>'. $p->nbre .'</td><td>'. $p->pu .'</td><td>'. $p->total .'</td></tr>';
         }
