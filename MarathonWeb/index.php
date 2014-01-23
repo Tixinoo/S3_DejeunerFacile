c7d6360551755024bef90ae8c1a8ad162259aa89
Tixinoo <antoine.nosal@gmail.com>
2014-01-23 12:54:47 +0100

    avancement index

--- MarathonWeb/index.php
+++ MarathonWeb/index.php
@@ -1,12 +1,13 @@
 <?php
 
-include_once 'SiteController.php';
+include_once 'control/SiteController.php';
 
 session_start();
 $c = new SiteController();
 
-if (isset($_GET['a']))
+if (isset($_GET['a'])) {
     $c->callAction($_GET);
+}
 else {
-     $c->callAction("default");
+    $c->callAction("default");
 }
