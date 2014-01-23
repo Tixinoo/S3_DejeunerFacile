618679ec05c920695e682fd7992c949f0d500f1d
Maxime <Maxime@Maximinou>
2014-01-23 11:14:46 +0100

    Test11

--- MarathonWeb/tests/plats_test.php
+++ MarathonWeb/tests/plats_test.php
@@ -6,3 +6,16 @@
  * and open the template in the editor.
  */
 
+include ('../Plat.php');
+// Création d'un plat 
+$p = new Plat();
+$p->nom = "test";
+$p->description = "Juste un test, il a l'air très bon";
+$p->insert();
+
+$liste = $p->findAll();
+foreach($liste as $key => $value) {
+    var_dump($value);
+}
+
+
