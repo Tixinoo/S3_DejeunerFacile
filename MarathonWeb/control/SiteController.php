c623a9b1c0d8d7bfe7e8c99114003bd4c9c02792
Tixinoo <antoine.nosal@gmail.com>
2014-01-23 12:53:53 +0100

    avancement sitecontroller

--- MarathonWeb/control/SiteController.php
+++ MarathonWeb/control/SiteController.php
@@ -1,9 +1,13 @@
 <?php
 
+include_once 'Controller.php';
+include_once 'base/Theme.php';
+include_once 'vue/Vue.php';
+
 class SiteController extends Controller {
 
     public function __construct() {
-        $this->tab = array(
+        $this->tab_action = array(
                     'listTheme'=>'listThemeAction',
                     'listResto'=>'listRestoAction',
                     'default'=>'defaultAction'
@@ -13,12 +17,15 @@ class SiteController extends Controller {
     public function defaultAction() {
         $themes = Theme::findAll();
         $vue = new Vue($themes);
-        $vue->vue_generale();
+        $vue->vue_all_theme();
     }
 
     public function listThemeAction() {
+        echo "Prout";
         $themes = Theme::findAll();
+        var_dump($themes);
         $vue = new Vue($themes);
+        echo "Prout";
         $vue->vue_all_theme();
     }
     
@@ -26,5 +33,11 @@ class SiteController extends Controller {
         $restos = Restaurant::findByTheme($idtheme);
         $vue = new Vue($restos);
         $vue->vue_all_resto($restos); 
+    }
+    
+    public function listPlatAction($idresto) {
+        $plats = Plat::findByResto($idresto);
+        $vue = new Vue($plats);
+        $vue->vue_all_plat($plats);
     }
 }
