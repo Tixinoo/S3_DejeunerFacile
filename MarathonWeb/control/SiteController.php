146895f7b5bbd0661578378d3c5fa3b4fa0bbaca
kernel57 <kernel2.6.31@gmail.com>
2014-01-23 13:09:14 +0100

    Kernel57

--- MarathonWeb/control/SiteController.php
+++ MarathonWeb/control/SiteController.php
@@ -3,6 +3,7 @@
 include_once 'Controller.php';
 include_once 'base/Theme.php';
 include_once 'vue/Vue.php';
+include_once 'base/Restaurant.php';
 
 class SiteController extends Controller {
 
@@ -39,5 +40,46 @@ class SiteController extends Controller {
         $plats = Plat::findByResto($idresto);
         $vue = new Vue($plats);
         $vue->vue_all_plat($plats);
+    }
+    
+    public function addPanier($plat) {
+        if (!isset($_SESSION['panier'])) {
+            $_SESSION['panier'] = array();
+            $_SESSION['panier']['plat'] = array();
+            $_SESSION['panier']['nbre'] = array();
+        }
+        if (isset($_SESSION['panier']['plat'][$plat->id])) {
+            $_SESSION['panier']['nbre'][$plat->id] += 1;
+        } else {
+            $_SESSION['panier']['plat'][$plat->id] = $plat;
+            $_SESSION['panier']['nbre'][$plat->id] = 1;
+        }
+    }
+
+    public function getDetailPanier() {
+        $res = NULL;
+        if (!isset($_SESSION['panier'])) {
+            $res = array();
+            $res['type'] = array();
+            $res['restaurant'] = array();
+            $res['plat'] = array();
+            $res['nbre'] = array();
+            $res['pu'] = array();
+            $res['total'] = array();
+            foreach ($_SESSION['panier']['plat'] as $plat) {
+                $res['type'][$plat->id] = Theme::findById(Restaurant::findById($plat->id_resto)->id_theme)->nom;
+                $res['restaurant'][$plat->id] = Restaurant::findById($plat->id_resto)->nom;
+                $res['plat'][$plat->id] = $plat->nom;
+                $res['nbre'][$plat->id] = $_SESSION['panier']['nbre'][$plat->id];
+                $res['pu'][$plat->id] = $plat->prix;
+                $res['total'][$plat->id] = $plat->prix * $_SESSION['panier']['nbre'][$plat->id];
+            }
+        }
+        return $res;
+    }
+
+    public function resetPanier() {
+        session_destroy();
+        session_start();
     }
 }
