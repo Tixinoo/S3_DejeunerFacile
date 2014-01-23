1141bb1dc8b3ae38c5b9dd95dc26cefb68671486
Tixinoo <antoine.nosal@gmail.com>
2014-01-23 13:10:52 +0100

    // Tixinoo

--- MarathonWeb/tests/theme_test.php
+++ MarathonWeb/tests/theme_test.php
@@ -4,12 +4,14 @@ include ('../base/Theme.php');
 $t = new Theme();
 $t->nom = "nom";
 $t->description = "description";
-var_dump($t);
-$t->insert();
-echo "".$t->id."";
+//var_dump($t);
+//$t->insert();
+//echo "".$t->id."";
 $liste = Theme::findAll();
+var_dump($liste);
 
 foreach ($liste as $key => $value)
 {
+    echo "coucou";
     var_dump($value);
 }
