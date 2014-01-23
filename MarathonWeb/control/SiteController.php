e34fc7497ed702572872e23fd1d51936b80f041f
kernel57 <kernel2.6.31@gmail.com>
2014-01-23 13:53:20 +0100

    Kernel57

--- MarathonWeb/control/SiteController.php
+++ MarathonWeb/control/SiteController.php
@@ -43,40 +43,4 @@ class SiteController extends Controller {
         $vue->vue_all_plat($plats);
     }
     
-    public function addPanier($plat, $nb=1) {
-        if (!isset($_SESSION['panier'])) {
-            $_SESSION['panier'] = array();
-            $_SESSION['panier']['plat'] = array();
-            $_SESSION['panier']['nbre'] = array();
-        }
-        $_SESSION['panier']['plat'][$plat->id] = $plat;
-        $_SESSION['panier']['nbre'][$plat->id] = $nb;
-    }
-
-    public function getDetailPanier() {
-        $res = NULL;
-        if (isset($_SESSION['panier'])) {
-            $res = array();
-            $res['type'] = array();
-            $res['restaurant'] = array();
-            $res['plat'] = array();
-            $res['nbre'] = array();
-            $res['pu'] = array();
-            $res['total'] = array();
-            foreach ($_SESSION['panier']['plat'] as $plat) {
-                $res['type'][$plat->id] = Theme::findById(Restaurant::findById($plat->id_resto)->id_theme)->nom;
-                $res['restaurant'][$plat->id] = Restaurant::findById($plat->id_resto)->nom;
-                $res['plat'][$plat->id] = $plat->nom;
-                $res['nbre'][$plat->id] = $_SESSION['panier']['nbre'][$plat->id];
-                $res['pu'][$plat->id] = $plat->prix;
-                $res['total'][$plat->id] = $plat->prix * $_SESSION['panier']['nbre'][$plat->id];
-            }
-        }
-        return $res;
-    }
-
-    public function resetPanier() {
-        session_destroy();
-        session_start();
-    }
 }
