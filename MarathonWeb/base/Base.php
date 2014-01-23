83f0b0ec0418465e322acad10b849b2aa70d31bc
Maxime <Maxime@Maximinou>
2014-01-23 10:57:15 +0100

    Test11

---
+++ MarathonWeb/base/Base.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Base {
+    
+    function __construct() {
+        //Constructeur vide
+    }
+    
+    public static function getConnection() {
+        try {
+            $ini_array = parse_ini_file("configbdd.ini");
+            $dsn= $ini_array['driver'] . ":host=" . $ini_array['host'] . ";dbname=" . $ini_array['dbname'];
+            $bdd = new PDO($dsn, $ini_array['user'], $ini_array['pass']);
+            return $bdd;
+        } catch (Exception $e) {
+            throw new BaseException("connection: $dsn ".$e->getMessage(). '<br/>');
+        }
+    }
+}
---
+++ MarathonWeb/base/Plat.php
@@ -0,0 +1,163 @@
+<?php
+
+include 'Base.php';
+
+class Plat {
+
+    // Attributs d'un plat
+    private $id, $nom, $description, $prix, $photo, $id_resto;
+
+    public function __construct() {
+        //Constructeur vide
+    }
+
+    // Getter magique !
+    public function __get($attr_name) {
+        if (property_exists(__CLASS__, $attr_name)) {
+            return $this->$attr_name;
+        }
+        $emess = __CLASS__ . ": unknown member $attr_name (getAttr)";
+        throw new Exception($emess, 45);
+    }
+
+    //Setter magique !
+    public function __set($attr_name, $attr_val) {
+
+        if (property_exists(__CLASS__, $attr_name)) {
+            $this->$attr_name = $attr_val;
+        }
+    }
+
+    /**
+     * Méthode qui met un jour un Plat dans la table
+     * @return type
+     * @throws Exception
+     */
+    public function update() {
+
+        if (!isset($this->id)) {
+            throw new Exception(__CLASS__ . ": Primary Key undefined : cannot update");
+        }
+
+        $c = Base::getConnection();
+
+        //$id, $nom, $description, $prix, $photo, $id_resto
+
+        $query = $c->prepare("update plats set nom= ?, description= ?, prix= ?, photo= ?, id_resto= ?
+				                   where id=?");
+
+        /*
+         * liaison des paramêtres : 
+         */
+        $query->bindParam(1, $this->nom, PDO::PARAM_STR);
+        $query->bindParam(2, $this->description, PDO::PARAM_STR);
+        $query->bindParam(3, $this->prix, PDO::PARAM_INT);
+        $query->bindParam(4, $this->photo, PDO::PARAM_STR);
+        $query->bindParam(5, $this->id_resto, PDO::PARAM_INT);
+        $query->bindParam(6, $this->id, PDO::PARAM_INT);
+
+        /*
+         * exécution de la requête
+         */
+
+        return $query->execute();
+    }
+    
+    /**
+     *   Suppression dans la base
+     *
+     *   Supprime la ligne dans la table corrsepondant à l'objet courant
+     *   L'objet doit posséder un OID
+     */
+    public function delete() {
+
+        if (!isset($this->id)) {
+            throw new Exception(__CLASS__ . ": Primary Key undefined : cannot update");
+        }
+
+        $c = Base::getConnection();
+
+
+        $query = $c->prepare("DELETE FROM plats where id=?");
+        $query->bindParam(1, $this->id, PDO::PARAM_INT);
+
+
+
+        return $query->execute();
+    }
+    
+    /**
+     * Ajoute un plat dans la base
+     */
+    public function insert() {
+
+        $c = Base::getConnection();
+
+
+        $query = $c->prepare("INSERT INTO plats (nom, description, prix, photo, id_resto) VALUES ( :titre, :desc, :prix, :photo, :id_resto)");
+        $query->bindParam(':titre', $this->nom, PDO::PARAM_STR);
+        $query->bindParam(':desc', $this->description, PDO::PARAM_STR);
+        $query->bindParam(':prix', $this->prix, PDO::PARAM_STR);
+        $query->bindParam(':photo', $this->photo, PDO::PARAM_STR);
+        $query->bindParam(':id_resto', $this->id_resto, PDO::PARAM_STR);
+
+        $query->execute();
+        //Il faut récupérer l'ID et le mettre dans l'objet
+
+        $this->id = $c->lastInsertId();
+    }
+	
+	public static function findById($id) {
+
+        $c = Base::getConnection();
+        $query = $c->prepare("select * from plats where id=?");
+        $query->bindParam(1, $id, PDO::PARAM_INT);
+        $dbres = $query->execute();
+
+
+        $d = $query->fetch(PDO::FETCH_BOTH);
+
+        $p = new Plat();
+
+        $p->id = $d['id'];
+        $p->nom = $d['titre'];
+        $p->description = $d['description'];
+		$p->prix = $d['titre'];
+		$p->photo = $d['titre'];
+		$p->id_resto = $d['titre'];
+
+        return $categorie;
+    }
+
+    /**
+     *   Finder All
+     *
+     *   Renvoie toutes les lignes de la table categorie
+     *   sous la forme d'un tableau d'objet
+     *  
+     *   @static
+     *   @return Array renvoie un tableau de categorie
+     */
+    public static function findAll() {
+
+
+        $res = array();
+
+        $c = Base::getConnection();
+        $query = $c->prepare("select * from categorie");
+        $dbres = $query->execute();
+
+        while ($d = $query->fetch(PDO::FETCH_BOTH)) {
+            $categorie = new Categorie();
+
+            $categorie->id = $d['id'];
+            $categorie->titre = $d['titre'];
+            $categorie->description = $d['description'];
+
+            $res[] = $categorie;
+        }
+
+        return $res;
+    }
+
+}
